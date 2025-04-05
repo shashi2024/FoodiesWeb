5b7b1d51f1fe01eafdd96192a8952eb1222116ee
Sashi2000 <sashinisithara20@gmail.com>
2025-04-05 14:11:14 +0530

    api integration

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,23 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 
-const App = () => {
+function App() {
+  const [message, setMessage] = useState("");
+
+  useEffect(() => {
+    axios
+      .get("/api/hello")
+      .then((response) => setMessage(response.data))
+      .catch((error) => console.error(error));
+  }, []);
+
   return (
     <div>
-      <h1 class="text-3xl font-bold underline">Hello world!</h1>
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <h1>Spring Boot & React App</h1>
+      <p>{message}</p>
     </div>
   );
-};
+}
 
 export default App;
--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,10 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss(),],
+  plugins: [react(), tailwindcss()],
+  server: {
+    proxy: {
+      '/api': 'http://localhost:8080', // This forwards requests to your Spring Boot backend
+    },
+  },
 })
