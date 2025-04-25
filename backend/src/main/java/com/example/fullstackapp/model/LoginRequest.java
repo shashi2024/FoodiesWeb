28b5fe8660482629723dde68e074ba4495f09287
pasindu110 <pasindusandeep2000@gmail.com>
2025-04-26 02:32:21 +0530

    Add login

---
+++ backend/src/main/java/com/example/fullstackapp/model/LoginRequest.java
@@ -0,0 +1,27 @@
+package com.perfectrecipe.model;
+
+public class LoginRequest {
+    private String email;
+    private String password;
+
+    // Default constructor
+    public LoginRequest() {
+    }
+
+    // Getters and setters
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+} 
